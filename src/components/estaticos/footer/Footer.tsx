a8b95e7078b5a72a13797834471949e33b285d9c
Marcos Vinicius <83991350+MarcosVxny@users.noreply.github.com>
2023-04-30 16:02:03 -0300

    Update Footer.tsx

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,45 +1,84 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
+import React from "react";
+
+import { Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import EmailIcon from "@material-ui/icons/Email";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
+import "./Footer.css";
+import { useSelector } from "react-redux";
+import { TokenState } from "../../../store/tokens/tokensReducer";
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  var footerComponent;
+
+  if (token != "") {
+    footerComponent = (
+      <Grid container className="container-centerow, gridcontainer-center">
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <Box paddingTop={3} className="box gridcontainer-center">
+              <Typography
+                variant="h5"
+                gutterBottom
+                className="box_text-align box_text-white"
+              >
+                Contate-me pelas redes sociais!
+              </Typography>
+            </Box>
+            <Box className="rodape">
+              <a href="https://github.com/am3rco" target="_blank">
+                <GitHubIcon className="icons" />
+              </a>
+              <a href="https://www.instagram.com/am3rco" target="_blank">
+                <InstagramIcon className="icons" />
+              </a>
+
+              <a href="https://mail.google.com" target="_blank">
+                <EmailIcon className="icons" />
+              </a>
+              <a
+                href="https://www.linkedin.com/school/generationbrasil/"
+                target="_blank"
+              >
+                <LinkedInIcon className="icons" />
+              </a>
+            </Box>
+          </Box>
+          <Box className="box2">
+            <Box paddingTop={1}>
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                className="box_text-align"
+              >
+                © 2023 Copyright:
+              </Typography>
+            </Box>
+            <Box>
+              <a target="_blank" href="https://github.com/am3rco">
+                <Typography
+                  variant="subtitle2"
+                  gutterBottom
+                  className="box_text-align box_text-black"
+                >
+                  Marco Antônio
+                </Typography>
+              </a>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  return <>{footerComponent}</>;
 }
 
 export default Footer;
