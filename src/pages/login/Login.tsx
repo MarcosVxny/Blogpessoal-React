5b793fbe8b12a222073e8514e9d8743bdebcaf45
Marcos Vinicius <83991350+MarcosVxny@users.noreply.github.com>
2023-04-30 16:07:18 -0300

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,101 +1,150 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Grid, Typography, TextField, Button } from '@material-ui/core';
-import {Box} from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
-import { login } from '../../services/Services';
-import UserLogin from '../../models/UserLogin';
-import './Login.css';
-import { useDispatch } from 'react-redux';
-import { addToken } from "../../store/tokens/actions";
-import { toast } from 'react-toastify';
+import React, { ChangeEvent, useState, useEffect } from "react";
+
+import "./Login.css";
+import { Grid, Typography, TextField, Button } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../../services/Services";
+import UserLogin from "../../models/UserLogins";
+import { useDispatch } from "react-redux";
+import { addToken } from "../../store/tokens/action";
+import { toast } from "react-toastify";
 
 function Login() {
-    let navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [token, setToken] = useState('');
-    const [userLogin, setUserLogin] = useState<UserLogin>(
-        {
-            id: 0,
-            nome: '',
-            usuario: '',
-            senha: '',
-            token: ''
-        }
-        )
+  let navigate = useNavigate(); // Redireciona o usuário para uma determinada página
 
-        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+  const dispatch = useDispatch();
 
-            setUserLogin({
-                ...userLogin,
-                [e.target.name]: e.target.value
-            })
-        }
+  // OuseLocalStorage() armazena e recupera um valor identificado pela chave 'token' no localStorage do navegador
+  const [token, setToken] = useState("");
 
-            useEffect(()=>{
-                if(token != ''){
-                    dispatch(addToken(token));
-                    navigate('/home')
-                }
-            }, [token])
+  // useState - Observa o estado da variável.
+  const [userLogin, setUserLogin] = useState<UserLogin>(
+    // Inicializa o estado local "userLogin" com um objeto da classe "UserLogin" contendo as propriedades "id", "usuario", "senha" e "token" com valores vazios
+    // "setUserLogin" é uma função que permite atualizar o valor do estado "userLogin" e re-renderizar o componente para exibir as alterações
+    {
+      id: 0,
+      usuario: "",
+      senha: "",
+      foto: "",
+      token: "",
+    }
+  );
 
-        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
-            e.preventDefault();
-            try{
-                await login(`/usuarios/logar`, userLogin, setToken)
-                toast.success('Usuário logado com sucesso!', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    theme: "colored",
-                    progress: undefined,
-                    });
-            }catch(error){
-                toast.error('Dados do usuário inconsistentes. Erro ao logar!', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    theme: "colored",
-                    progress: undefined,
-                    });
-            }
-        }
+  // A função updateModel com o parâmetro e: ChangeEvent<HTMLInputElement> é utilizada para lidar com eventos de mudança em elementos de formulário do tipo input e atualizar o modelo de dados da aplicação com base no valor atualizado.
+  function updateModel(e: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin, // Está utiliza o operador spread para criar uma cópia do estado userLogin antes de atualizá-lo, afim de não perder nenhuma propriedade durante a att
+      [e.target.name]: e.target.value,
+      //[e.target.name]: nome da propriedade para identificar qual é o campo
+      //e.target.value: pega o valor digitado no campo
+    });
+  }
 
-    return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid alignItems='center' xs={6}>
-                <Box paddingX={20}>
-                    <form onSubmit={onSubmit}>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
-                        <Box marginTop={2} textAlign='center'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                        </Box>
-                    </form>
-                    <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-                        </Box>
-                        <Link to='/cadastrousuario'>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
-                        </Link>
-                            
-                    </Box>
-                </Box>
-            </Grid>
-            <Grid xs={6} className='imagem'>
+  useEffect(
+    () => {
+      // useEffect - executa uma função sempre que uma variavel dofre uma alteração
+      if (token != "") {
+        dispatch(addToken(token));
+        navigate("/home"); // se o token for diferente de vazio, ira redirecionar para a home;
+      }
+    },
+    [token] // sempre que o token for modificado, o useEffect é acionado
+  );
 
-            </Grid>
-        </Grid>
-    );
+  // Esta função é ativada quando o formulário é enviado e impede o comportamento padrão do navegador de enviar o formulário.
+  // Em vez disso, exibe uma mensagem de log no console com informações do objeto userLogin.
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      await login("usuarios/logar", userLogin, setToken);
+
+      toast.success("Usucario logado com sucesso", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "colored",
+      });
+    } catch (error) {
+      toast.error("Dados do usuário inconsistentes. Erro ao logar.", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  }
+
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid xs={6} alignItems="center">
+        <Box paddingX={20}>
+          <form onSubmit={onSubmit}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              align="center"
+              className="negrito"
+            >
+              Entrar
+            </Typography>
+            <TextField
+              value={userLogin.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+              id="usuario"
+              label="Nome do Usuario"
+              variant="outlined"
+              name="usuario"
+              margin="normal"
+              fullWidth
+            ></TextField>
+            <TextField
+              value={userLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+              id="senha"
+              label="Digite sua Senha"
+              variant="outlined"
+              name="senha"
+              margin="normal"
+              fullWidth
+              type="password"
+            ></TextField>
+
+            <Box marginTop={2} textAlign="center">
+              <Button type="submit" variant="contained" className="botao">
+                Logar
+              </Button>
+            </Box>
+          </form>
+
+          <Box display="flex" justifyContent="center" marginTop={2}>
+            <Box marginRight={1}>
+              <Typography variant="subtitle1" gutterBottom align="center">
+                Não possui uma conta?
+              </Typography>
+            </Box>
+
+            <Link to="/cadastrousuario" style={{ color: "black" }}>
+              <Typography variant="subtitle1" gutterBottom className="cadastro">
+                Cadastre-se
+              </Typography>
+            </Link>
+          </Box>
+        </Box>
+      </Grid>
+
+      <Grid xs={6} className="bg-image"></Grid>
+    </Grid>
+  );
 }
 
 export default Login;
